506eee52df97dbcc90837ad3dbc99894cafa440d
jmeridth <jmeridth@gmail.com>
2022-11-17 09:10:56 -0600

    move to react

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React, { useState } from 'react';
 
 
 function App() {
-  const [count, setCount] = useState(10);
-  const [min, setMin] = useState(1);
-  const [max, setMax] = useState(100);
+  const defaults = { "min": 1, "max": 100, "count": 10 };
+  const [count, setCount] = useState(defaults["count"]);
+  const [min, setMin] = useState(defaults["min"]);
+  const [max, setMax] = useState(defaults["max"]);
   const [result, setResult] = useState([]);
   const [resultVisible, setResultVisible] = useState(false);
 
@@ -13,30 +14,54 @@ function App() {
     setResultVisible(false);
     var results = [];
     for (let i = 0; i < count; i++) {
-      var number = Math.floor(Math.random() * max) + min;
+      var number = Math.floor(Math.random() * (max - min)) + min;
       results.push(number);
     }
     setResultVisible(true);
     setResult(results);
   }
 
+  const handleCountChange = (event, fieldName, setFunc) => {
+    if (event.target.value > 0) {
+      setFunc(+event.target.value);
+    } else {
+      setFunc(defaults[fieldName]);
+    }
+  }
+
 
   return (
     <div className="App">
-      <label name="min" htmlFor="min">Min</label>
-      <input id="min" type="text" name="min" value={min} onChange={e => setMin(e.target.value)} />
-      <p><small>default is 1</small></p>
-      <label name="max" htmlFor="max">Max</label>
-      <input id="max" type="text" name="max" value={max} onChange={e => setMax(e.target.value)} />
-      <p><small>default is 100</small></p>
-      <label name="count" htmlFor="count">Count</label>
-      <input id="count" type="text" name="count" value={count} onChange={e => setCount(e.target.value)} />
-      <p><small>default is 10</small></p>
-
-      <button onClick={getRandomNumbers}>Generate</button>
-      <p>
+      <div>
         <strong>{resultVisible && result.join(", ")}</strong>
-      </p>
+      </div>
+      <div>
+        <label name="min" htmlFor="min">Min</label>
+      </div>
+      <div>
+        <input id="min" type="text" name="min" value={min}
+          onChange={e => handleCountChange(e, "min", setMin)} />
+      </div>
+
+      <div>
+        <label name="max" htmlFor="max">Max</label>
+      </div>
+      <div>
+        <input id="max" type="text" name="max" value={max}
+          onChange={e => handleCountChange(e, "max", setMax)} />
+      </div>
+
+      <div>
+        <label name="count" htmlFor="count">Count</label>
+      </div>
+      <div>
+        <input id="count" type="text" name="count" value={count}
+          onChange={e => handleCountChange(e, "count", setCount)} />
+      </div>
+
+      <div>
+        <button onClick={getRandomNumbers}>Generate</button>
+      </div>
     </div>
   );
 }
